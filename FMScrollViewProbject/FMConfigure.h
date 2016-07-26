23065e1824a4d71a277f6810e94d3e694983cc3a
WangGang <wangli2317@vip.qq.com>
2016-07-26 17:43:18 +0800

    提交代码

--- FMScrollViewProbject/FMConfigure.h
+++ FMScrollViewProbject/FMConfigure.h
@@ -23,9 +23,6 @@
 #define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
 #define IOS_VERSION [[[UIDevice currentDevice] systemVersion] floatValue]
 
-#define URL @""
-
-
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 #define UIColorFromRGBALPHA(rgbValue,alphaNum) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:alphaNum]
